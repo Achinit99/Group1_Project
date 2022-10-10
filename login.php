4b1fd6c4ff0ce8966508f86fd71dc778bcc7cce0
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-10-10 15:01:08 +0530

    Create admin

---
+++ login.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="loin.css" />
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body style="background-color: cadetblue" class="mr-3">
+    <section class="vh-100" style="background-color: #508bfc;">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
+
+                            <h3 class="mb-5"><i class="bi bi-person-bounding-box " style="font-size: 5rem;"></i></h3>
+
+                            <div class="form-outline mb-4">
+                                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Username" />
+
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" />
+
+                            </div>
+
+                            <!-- Checkbox -->
+                            <div class="form-check d-flex justify-content-start mb-4">
+                                <input class="form-check-input me-2" type="checkbox" value="" id="form1Example3" />
+                                <label class="form-check-label" for="form1Example3"> Remember password </label>
+                            </div>
+
+                            <button type=" button" class="btn btn-outline-dark me-3">LOGIN<i class="bi bi-box-arrow-in-right"></i></button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+</body>
+
+</html>
