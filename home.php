528ad8077c917b29786671ef5577424662f69a15
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-10-28 23:48:27 +0530

    new nav bar

--- home.php
+++ home.php
@@ -16,7 +16,7 @@
 
 
 
-    <!-- Start Top Nav -->
+<!-- 
     <nav class="navbar navbar-expand-lg bg-danger navbar-light d-none d-lg-block" id="templatemo_nav_top">
         <div class="container text-light">
             <div class="w-100 d-flex justify-content-between">
@@ -32,7 +32,7 @@
             </div>
         </div>
     </nav>
-    <!-- Close Top Nav -->
+   
 
 
 
@@ -69,13 +69,60 @@
     </form>
     </div>
     </nav>
+ -->
 
 
 
 
 
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active h5" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h5" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="bi bi-bag-heart h5"></i></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="user.jpg" class="rounded-circle" height="25px" width="25px" alt="Cinque Terre">
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">LOG OUT</a></li>
+          </ul>
+        </li>
+        
+        <!-- <li class="nav-item">
+        <i class="bi bi-bell text-black"></i>
+        </li> -->
+                    
 
+      </ul>
+    
+      <form class="d-flex">
+  
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
 
+        <!-- <i class="bi bi-bell text-white"></i>
+                    <a href="cart.php"><i class="bi bi-cart text-black "></i></a> -->
+      </form>
+    
+    </div>
+  </div>
+</nav>
 
 
 
