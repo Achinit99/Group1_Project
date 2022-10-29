d20978481c6a7865c46be56dded68030b285b631
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-10-29 11:42:33 +0530

    fix nav nad add new carosual

--- home.php
+++ home.php
@@ -10,11 +10,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="homestyle.css" rel="stylesheet">
     <title>Hello, world!</title>
+    <style>
+      /* @media only screen and (max-width: 991px) {
+  .navmobile {
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+  }
+} */
+    </style>
 </head>
 
 <body>
-
-
 
 
  <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light ">
@@ -23,7 +30,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse justify-content-end navmobile" id="navbarSupportedContent">
       <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active h5 mx-2" aria-current="page" href="#">Home</a>
@@ -48,122 +55,105 @@
             <li><a class="dropdown-item" href="#">LOG OUT</a></li>
           </ul>
         </li>
-  
 
-      </ul>
-    
-      <form class="d-flex">
-  
+        <li class="nav-item">
+        <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </li>
 
-        <!-- <i class="bi bi-bell text-white"></i>
-                    <a href="cart.php"><i class="bi bi-cart text-black "></i></a> -->
-      </form>
+      </ul>
+     
     
     </div>
   </div>
 </nav>
 
 
-
- <!-- Carousel wrapper -->
-<div id="carouselMaterialStyle" class="carousel slide carousel-fade" data-mdb-ride="carousel">
-  <!-- Indicators -->
+<div id="carouselExampleIndicators" class="carousel slide " data-bs-ride="carousel">
   <div class="carousel-indicators">
-    <button type="button" data-mdb-target="#carouselMaterialStyle" data-mdb-slide-to="0" class="active" aria-current="true"
-      aria-label="Slide 1"></button>
-    <button type="button" data-mdb-target="#carouselMaterialStyle" data-mdb-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-mdb-target="#carouselMaterialStyle" data-mdb-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-
-  <!-- Inner -->
-  <div class="carousel-inner rounded-5 shadow-4-strong">
-    <!-- Single item -->
+  <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="1.jpeg" class="d-block w-100"
-        alt="Sunset Over the City" />
-      <div class="carousel-caption d-none d-md-block">
-        <h2>First slide label</h2>
-        <button type="button" class="btn btn-primary btn-lg">Large button</button>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </div>
+      <img src="1.jpeg" class="d-block w-100" alt="...">
     </div>
-
-    <!-- Single item -->
     <div class="carousel-item">
-      <img src="1.jpeg" class="d-block w-100"
-        alt="Canyon at Nigh" />
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
+      <img src="2.jpeg" class="d-block w-100" alt="...">
     </div>
-
-    <!-- Single item -->
     <div class="carousel-item">
-      <img src="1.jpeg" class="d-block w-100"
-        alt="Cliff Above a Stormy Sea" />
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </div>
+      <img src="3.jpeg" class="d-block w-100" alt="...">
     </div>
   </div>
-  <!-- Inner -->
-
-  <!-- Controls -->
-  <button class="carousel-control-prev" type="button" data-mdb-target="#carouselMaterialStyle" data-mdb-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-mdb-target="#carouselMaterialStyle" data-mdb-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-<!-- Carousel wrapper -->
 
 
 <!-- cards start -->
 
-<div class="row m-5">
-  <div class="col-sm-3">
-    <div class="card">
+<div class="row m-12 mx-4 my-2 ">
+<div class="card mb-3" style="max-width: 340px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="free.jpeg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">FREE SHIPPING</h5>
+        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>  -->
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
+</div>
+  <div class="card mb-3" style="max-width: 340px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>  -->
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
+</div>
+<div class="card mb-3" style="max-width: 340px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>  -->
       </div>
     </div>
   </div>
-  <div class="col-sm-3">
-    <div class="card">
+</div>
+<div class="card mb-3" style="max-width: 340px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <!-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>  -->
       </div>
     </div>
   </div>
+</div>
 </div>
 
 
