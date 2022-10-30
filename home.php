661b4fd986cacf791a50c0d39612cd74bf0402fa
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-10-30 11:09:03 +0530

    nav bar shadow  & cart
    add cart button to home page product cards

--- home.php
+++ home.php
@@ -224,13 +224,13 @@ include_once('connection.php');
             <div
               class="bg-info rounded-circle d-flex align-items-center justify-content-center shadow-1-strong"
               style="width: 35px; height: 35px;">
-              <p class="text-white mb-0 small">x4</p>
+              <a href="#" class="text-white mb-0 small"><i class="bi bi-cart-plus-fill"></i></a>
             </div>   
           </div>
           <?php echo "<img src='uploaded_img/".$row['image']."' >"; ?>
           <div class="card-body">
             <div class="d-flex justify-content-between">
-              <p class="small"><a href="#!" class="text-muted"><?php echo $row['item_name']; ?></a></p>
+              <p class="small"><a href="#!" class="text-muted"><?php echo $row['manufacturer']; ?></a></p>
               <p class="small text-danger"><s>$1099</s></p>
             </div>
 
@@ -240,7 +240,7 @@ include_once('connection.php');
             </div>
 
             <div class="d-flex justify-content-between mb-2">
-              <p class="text-muted mb-0">Available: <span class="fw-bold">6</span></p>
+              <p class="text-muted mb-0">Available: <span class="fw-bold"><?php echo $row['quantity']; ?></span></p>
               <div class="ms-auto text-warning">
                 <i class="fa fa-star"></i>
                 <i class="fa fa-star"></i>
@@ -323,7 +323,7 @@ include_once('connection.php');
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
           <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
+            <i class="fas fa-gem me-3"></i>Alpha Computer
           </h6>
           <p>
             Here you can use rows and columns to organize your footer content. Lorem ipsum
@@ -378,10 +378,10 @@ include_once('connection.php');
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
           <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p><i class="fas fa-home me-3"></i> Kurunegala , Sri Lanka</p>
           <p>
             <i class="fas fa-envelope me-3"></i>
-            info@example.com
+            info@alpha.com
           </p>
           <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
           <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
