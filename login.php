38625434a940d9867f3d159b341421c0caae2432
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-11-05 23:53:30 +0530

    log in rederection

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
- session_start(); 
+session_start();
 
 include_once('connection.php');
 if (isset($_POST['submit'])) {
@@ -24,12 +24,48 @@ if (isset($_POST['submit'])) {
 
         $result = mysqli_query($connection, $sql);
 
+
+        $sql2 = "SELECT roll FROM user 
+        WHERE name='{$uname}'
+        AND password='{$password}'
+        LIMIT 1";
+
+
+
+        $result2 = mysqli_query($connection, $sql2);
+
+        if (mysqli_num_rows($result2) > 0) {
+            // OUTPUT DATA OF EACH ROW
+            while ($row = mysqli_fetch_assoc($result2)) {
+                $roll= $row["roll"];
+            
+                   
+            }
+        } else {
+            echo "0 results";
+        }
+
+
+
+
+
+
+
+
+
         if ($result) {
 
             if (mysqli_num_rows($result) == 1) {
                 $_SESSION['user'] = $uname;
-                header('Location:index2.php');
-        
+                if($roll==1)
+                {
+                    header('Location:index.php');
+                }
+                else
+                {
+                    header('Location:index2.php');
+                }
+                
             } else {
                 $error[] = 'username and password invalid';
             }
@@ -40,7 +76,7 @@ if (isset($_POST['submit'])) {
         $error[] = 'PASSWORD is invalid or empty';
     }
 } // function that Destroys Session 
-  
+
 ?>
 
 
@@ -54,7 +90,7 @@ if (isset($_POST['submit'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
-        h3{
+        h3 {
             text-align: center;
         }
     </style>
@@ -65,44 +101,46 @@ if (isset($_POST['submit'])) {
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <h3 class="mb-5"><img src="logo2.png" width="140px" height="170px" alt=""></h3>
+                    <h3 class="mb-5"><img src="logo2.png" width="140px" height="170px" alt=""></h3>
                     <!-- <div class="card shadow-2-strong" style="border-radius: 1rem;"> -->
-                        <div class="card-body p-5 text-center" style="background-color: #191C24;">
+                    <div class="card-body p-5 text-center" style="background-color: #191C24;">
 
-                            <?php
-                            if (isset($error) && !empty($error)) {
-                                echo '<p style="color:red;">Empty username or passwors </p>';
-                            }
-                            ?>
+                        <?php
+                        if (isset($error) && !empty($error)) {
+                            echo '<p style="color:red;">Empty username or passwors </p>';
+                        }
+                        ?>
 
-                            <!-- <h3 class="mb-5"><img src="logo2.png" width="100px" height="120px" alt=""></h3> -->
-                            <form action="login.php" method="post">
-                                <div class="form-outline mb-4">
+                        <!-- <h3 class="mb-5"><img src="logo2.png" width="100px" height="120px" alt=""></h3> -->
+                        <form action="login.php" method="post">
+                            <div class="form-outline mb-4">
 
-                                    <input type="text" name="uname" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Username" />
+                                <input type="text" name="uname" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Username" />
 
-                                </div>
+                            </div>
 
-                                <div class="form-outline mb-4">
-                                    <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" />
+                            <div class="form-outline mb-4">
+                                <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" />
 
-                                </div>
+                            </div>
 
-                                <!-- Checkbox -->
-                                <div class=" mb-4">
-                                    <!-- <input class="form-check-input me-2" type="checkbox" value="" id="form1Example3" /> -->
-                                    <!-- <label class="form-check-label text-light" for="form1Example3"> Remember password </label> -->
-                                    <a href="register.php"><p class="text-light">Register now?</p></a>
-                                    
-                                </div>
-                               
+                            <!-- Checkbox -->
+                            <div class=" mb-4">
+                                <!-- <input class="form-check-input me-2" type="checkbox" value="" id="form1Example3" /> -->
+                                <!-- <label class="form-check-label text-light" for="form1Example3"> Remember password </label> -->
+                                <a href="register.php">
+                                    <p class="text-light">Register now?</p>
+                                </a>
 
-                                <button name="submit" type=" button" class="btn btn-outline-light me-3" href="login.php">LOGIN<i class="bi bi-box-arrow-in-right"></i></button>
-                            
-                        </div>
+                            </div>
+
+
+                            <button name="submit" type=" button" class="btn btn-outline-light me-3" href="login.php">LOGIN<i class="bi bi-box-arrow-in-right"></i></button>
+
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     </section>
 
--- register.php
+++ register.php
@@ -1,21 +1,15 @@
 <?php
- session_start(); 
 
 include_once('connection.php');
-if (isset($_POST['submit2'])) {
-   $user_name = $_POST['uname'];
-   $password = $_POST['password'];
-   $password2 = $_POST['password2'];
-   
-   if($password == $password2){
-    $insert = "INSERT INTO user(name, password) VALUES('$user_name', '$password')";
-    $add = mysqli_query($connection,$insert);
-   
 
-}
-else{
-    $message[] = 'please fill out all';
-}
+if(isset($_POST['submit2'])){
+
+    $uname = $_POST['uname'];
+    $password = $_POST['password'];
+
+    $insert = "INSERT INTO user(name, password) VALUES('$uname', '$password')";
+    $upload = mysqli_query($connection,$insert);
+    
 }
 
 ?>
@@ -29,69 +23,57 @@ else{
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
-        h3{
+        h3 {
             text-align: center;
         }
     </style>
 </head>
 
 <body style="background-color: cadetblue" class="mr-3">
+
+
     <section class="vh-100" style="background-color: #CF0A0A;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                <h3 class="mb-5"><img src="logo2.png" width="140px" height="170px" alt=""></h3>
-                <?php
+                    <h3 class="mb-5"><img src="logo2.png" width="140px" height="170px" alt=""></h3>
+                   
+                    
+                    <div class="card-body p-5 text-center" style="background-color: #191C24;">
 
-                    if(isset($message)){
-                    foreach($message as $msg){
-                    echo '<h6 class="mb-4 text-danger ">'.$msg.'</h6>';
+                       
+                        <form action="register.php" method="post">
+                            <div class="form-outline mb-4">
 
-                        }
-                    }
+                                <input type="text" name="uname" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Username" />
 
-?>
-                    <!-- <div class="card shadow-2-strong" style="border-radius: 1rem;"> -->
-                        <div class="card-body p-5 text-center" style="background-color: #191C24;">
+                            </div>
 
-                            <?php
-                            if (isset($error) && !empty($error)) {
-                                echo '<p style="color:red;">Empty username or passwors </p>';
-                            }
-                            ?>
+                            <div class="form-outline mb-4">
+                                <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" />
 
-                            <!-- <h3 class="mb-5"><img src="logo2.png" width="100px" height="120px" alt=""></h3> -->
-                            <form action="register.php" method="post">
-                                <div class="form-outline mb-4">
+                            </div>
+                            <div class="form-outline mb-4">
+                                <input type="password" name="password2" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Re-Password" />
 
-                                    <input type="text" name="uname" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Username" />
+                            </div>
 
-                                </div>
+                           
+                            <div class=" mb-4">
+                              
+                                <a href="login.php">
+                                    <p class="text-light">Login now?</p>
+                                </a>
 
-                                <div class="form-outline mb-4">
-                                    <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Password" />
+                            </div>
 
-                                </div>
-                                <div class="form-outline mb-4">
-                                    <input type="password" name="password2" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Re-Password" />
 
-                                </div>
+                            <button name="submit2" type=" button" class="btn btn-outline-light me-3" href="register.php">Register<i class="bi bi-box-arrow-in-right"></i></button>
 
-                                <!-- Checkbox -->
-                                <div class=" mb-4">
-                                    <!-- <input class="form-check-input me-2" type="checkbox" value="" id="form1Example3" /> -->
-                                    <!-- <label class="form-check-label text-light" for="form1Example3"> Remember password </label> -->
-                                    <a href="login.php"><p class="text-light">Login now?</p></a>
-                                    
-                                </div>
-                               
-
-                                <button name="submit2" type=" button" class="btn btn-outline-light me-3" href="register.php">Register<i class="bi bi-box-arrow-in-right"></i></button>
-                            
-                        </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     </section>
 
