cff799accbae79453030ef4bf972ec09f0b6a133
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-12-19 20:35:20 +0530

    Update customerprofile.php

--- customerprofile.php
+++ customerprofile.php
@@ -7,7 +7,7 @@ include_once('connection.php');
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
