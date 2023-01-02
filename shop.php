d2d794b4ea394b4b27be27167a75926b16f178b6
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-01-02 17:15:09 +0530

    Update shop.php

--- shop.php
+++ shop.php
@@ -27,6 +27,10 @@ if(isset($_POST['add_to_cart'])){
     }
 }
 
+
+
+require_once("dbcontroller.php");
+$db_handle = new DBController();
 ?>
 
 <!doctype html>
@@ -148,6 +152,39 @@ if(isset($_POST['add_to_cart'])){
     <!-- 
 section end -->
 
+
+
+
+<!-- new product section start -->
+
+
+<div id="product-grid">
+	<div class="txt-heading">Products</div>
+	<?php
+	$product_array = $db_handle->runQuery("SELECT * FROM item ORDER BY id ASC");
+	if (!empty($product_array)) { 
+		foreach($product_array as $key=>$value){
+	?>
+		<div class="product-item">
+			<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["item_code"]; ?>">
+			<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
+			<div class="product-tile-footer">
+			<div class="product-title"><?php echo $product_array[$key]["manufacturer"]; ?></div>
+			<div class="product-price"><?php echo "$".$product_array[$key]["item_name"]; ?></div>
+			<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+			</div>
+			</form>
+		</div>
+	<?php
+		}
+	}
+	?>
+
+    <h1>test</h1>
+</div>
+
+
+<!-- new prodoct end -->
 
 
     <dic class="foo"></dic>
