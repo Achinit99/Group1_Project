e98db028c6e28826c856a8efcb38413fb0211bbd
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-01-17 22:01:22 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php   
-session_start(); //to ensure you are using same session
+session_start(); 
 session_destroy(); //destroy the session
 header("location:/group1/login.php"); //to redirect back to "index.php" after logging out
 exit();
